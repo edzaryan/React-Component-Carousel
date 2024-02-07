cdb2f0b033b5e606dc4b375df99129dc19877e68
Edgar Yeghiazaryan <edzaryan@gmail.com>
2024-02-07 17:00:11 +0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="slider">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/SliderButton/SliderButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SliderButton = ({ icon }) => {
+    return (
+        <div>
+            { icon }
+        </div>
+    );
+};
+
+export default SliderButton;
